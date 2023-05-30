148c5163aecfbea8ef63250e1b8b2189f0d0e1fc
KittyLegacy <1708jld@gmail.com>
2023-05-30 15:42:52 +0800

    respond to pultiple pings

--- app/main.js
+++ app/main.js
@@ -6,8 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
 //   // Handle connection
-    connection.on("data", (data) => {
-        console.log(data)
+    connection.on("data", () => {
         connection.write("+PONG\r\n");
     })
 });
